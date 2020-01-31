2120b2ee25161907374bda4e6b5896f75db2e39f
LAPTOP-C8F0QODP\Joël <Jsmjohn@hotmail.com>
2020-01-31 21:00:34 +0100

    SoundManger + Scene

---
+++ Thyme 2.0/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header("Managers")]
+    public SoundMan soundMan;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
