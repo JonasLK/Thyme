e17ca30b8bcd6fd6e64947f6acafb6e7b16d0360
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-01-31 11:21:27 +0100

    health script part 1

--- Thyme 2.0/Assets/Scripts/UI/PlayerHealth.cs
+++ Thyme 2.0/Assets/Scripts/UI/PlayerHealth.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject h;
+    public Image health;
+    public Sprite healthBar;
+    // player health script here
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey("a"))
+        {
+            h.transform.localScale = new Vector3(-1, 1, 1);
+            PlayerTakesDamage();
+        }
+    }
+    
+    public void PlayerTakesDamage()
+    {
+        /* healthBar.scale.x = current player health : max player health;
+         */
+        if(h.transform.localScale.x < 0)
+        {
+            h.transform.localScale = new Vector3(0,1,1);
+            //death code
+        }
     }
 }
