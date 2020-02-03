f9a8e75552a452baacf16f66ed378629112a3d4b
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-02-03 10:18:24 +0100

    start of deathdrop script

---
+++ Thyme 2.0/Assets/Scripts/Combat/Enemies/DeathDrops.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathDrops: MonoBehaviour
+{
+    public int maxMoneyDrop;
+    public int minMoneyDrop;
+    public int timeCrystalChargeAmount;
+    public GameObject cogs;
+
+    void Start()
+    {
+        
+    }
+    
+    public void DeathDrop(GameObject attachedEnemy)
+    {
+        Instantiate(cogs, attachedEnemy.transform.position, Quaternion.identity);
+
+    }
+}
