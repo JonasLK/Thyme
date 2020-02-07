c9469d2c9d5dd72f6ef3f1e56fedd6c1777d39fb
Jonathan Breeschooten <36034511+ZeroLaser1337@users.noreply.github.com>
2020-02-07 13:16:47 +0100

    I made Chase script lol

---
+++ Thyme 2.0/Assets/Models/Enemy/Chase.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public float range;
+    public float speed;
+    public float angel;
+    public float attackRange;
+    public Transform player;
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = player.position - transform.position;
+        float angle = Vector3.Angle(dir,transform.forward);
+        if (Vector3.Distance(player.position, transform.position) < range && angle < angel)
+        {
+            dir.y = 0;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                     Quaternion.LookRotation(dir), 0.1f);
+            Debug.Log(transform.name + "IdleOff");
+            if(dir.magnitude > attackRange)
+            {
+                Debug.Log(transform.name + "Chase");
+                transform.Translate(0, 0, speed);
+                ResetAnime();
+                anim.SetTrigger("isChasing");
+            }
+            else
+            {
+                Debug.Log(transform.name + "Attacking");
+                ResetAnime();
+                anim.SetTrigger("isAttacking");
+            }
+          
+        }
+        else
+        {
+            Debug.Log(transform.name + "IdleOn");
+            ResetAnime();
+            anim.SetTrigger("isIdle");
+        }
+
+    }
+
+    public void ResetAnime()
+    {
+        anim.ResetTrigger("isIdle");
+        anim.ResetTrigger("isChasing");
+        anim.ResetTrigger("isAttacking");
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
