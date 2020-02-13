c4d4a471d7d61c8ad6cae9c8189320f453681343
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-02-13 09:52:43 +0100

    tymecrystals revamped

--- Thyme 2.0/Assets/Scripts/Player/PlayerPickUp.cs
+++ Thyme 2.0/Assets/Scripts/Player/PlayerPickUp.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class PlayerPickUp : MonoBehaviour
 {
-    public float test;
     public float tymeCharge;
     public float amountOfTymeCrystals;
     public float portalCloseCost; //amount of tymecharge needed to fill 1 crystal
-    public GameObject tymeCrystal1, tymeCrystal2, tymeCrystal3;
+    public List<GameObject> tymeCrystals;
     public string pickUpTagName;
 
     public void Start()
     {
-        tymeCrystal1.transform.localScale = new Vector3(tymeCrystal1.transform.localScale.x, 0, tymeCrystal1.transform.localScale.z);
-        tymeCrystal2.transform.localScale = new Vector3(tymeCrystal2.transform.localScale.x, 0, tymeCrystal2.transform.localScale.z);
-        tymeCrystal3.transform.localScale = new Vector3(tymeCrystal3.transform.localScale.x, 0, tymeCrystal3.transform.localScale.z);
+        tymeCrystals[0].transform.localScale = new Vector3(tymeCrystals[0].transform.localScale.x, 0, tymeCrystals[0].transform.localScale.z);
+        tymeCrystals[1].transform.localScale = new Vector3(tymeCrystals[1].transform.localScale.x, 0, tymeCrystals[1].transform.localScale.z);
+        tymeCrystals[2].transform.localScale = new Vector3(tymeCrystals[2].transform.localScale.x, 0, tymeCrystals[2].transform.localScale.z);
     }
 
     public void OnTriggerEnter(Collider o)
@@ -28,29 +27,28 @@ public class PlayerPickUp : MonoBehaviour
 
     public void Update()
     {
-        if(tymeCharge > amountOfTymeCrystals * portalCloseCost)
+        if(tymeCharge > tymeCrystals.Capacity * portalCloseCost)
         {
-            tymeCharge = amountOfTymeCrystals * portalCloseCost;
+            tymeCharge = tymeCrystals.Capacity * portalCloseCost;
         }
 
         if(tymeCharge <= portalCloseCost)
         {
-            test = tymeCharge/portalCloseCost;
-            tymeCrystal1.transform.localScale = new Vector3(tymeCrystal1.transform.localScale.x, tymeCharge/portalCloseCost, tymeCrystal1.transform.localScale.z);
-            tymeCrystal2.transform.localScale = new Vector3(tymeCrystal2.transform.localScale.x, 0, tymeCrystal2.transform.localScale.z);
-            tymeCrystal3.transform.localScale = new Vector3(tymeCrystal3.transform.localScale.x, 0, tymeCrystal3.transform.localScale.z);
+            tymeCrystals[0].transform.localScale = new Vector3(tymeCrystals[0].transform.localScale.x, tymeCharge/portalCloseCost, tymeCrystals[0].transform.localScale.z);
+            tymeCrystals[1].transform.localScale = new Vector3(tymeCrystals[1].transform.localScale.x, 0, tymeCrystals[1].transform.localScale.z);
+            tymeCrystals[2].transform.localScale = new Vector3(tymeCrystals[2].transform.localScale.x, 0, tymeCrystals[2].transform.localScale.z);
         }
         else if(tymeCharge > portalCloseCost && tymeCharge <= (portalCloseCost*2))
         {
-            tymeCrystal1.transform.localScale = new Vector3(tymeCrystal1.transform.localScale.x, 1, tymeCrystal1.transform.localScale.z);
-            tymeCrystal2.transform.localScale = new Vector3(tymeCrystal2.transform.localScale.x, (tymeCharge-portalCloseCost)/portalCloseCost, tymeCrystal2.transform.localScale.z);
-            tymeCrystal3.transform.localScale = new Vector3(tymeCrystal3.transform.localScale.x, 0, tymeCrystal3.transform.localScale.z);
+            tymeCrystals[0].transform.localScale = new Vector3(tymeCrystals[0].transform.localScale.x, 1, tymeCrystals[0].transform.localScale.z);
+            tymeCrystals[1].transform.localScale = new Vector3(tymeCrystals[1].transform.localScale.x, (tymeCharge-portalCloseCost)/portalCloseCost, tymeCrystals[1].transform.localScale.z);
+            tymeCrystals[2].transform.localScale = new Vector3(tymeCrystals[2].transform.localScale.x, 0, tymeCrystals[2].transform.localScale.z);
         }
         else if(tymeCharge > portalCloseCost * 2)
         {
-            tymeCrystal1.transform.localScale = new Vector3(tymeCrystal1.transform.localScale.x, 1, tymeCrystal1.transform.localScale.z);
-            tymeCrystal2.transform.localScale = new Vector3(tymeCrystal2.transform.localScale.x, 1, tymeCrystal2.transform.localScale.z);
-            tymeCrystal3.transform.localScale = new Vector3(tymeCrystal3.transform.localScale.x, (tymeCharge - portalCloseCost*2)/portalCloseCost, tymeCrystal3.transform.localScale.z);
+            tymeCrystals[0].transform.localScale = new Vector3(tymeCrystals[0].transform.localScale.x, 1, tymeCrystals[0].transform.localScale.z);
+            tymeCrystals[1].transform.localScale = new Vector3(tymeCrystals[1].transform.localScale.x, 1, tymeCrystals[1].transform.localScale.z);
+            tymeCrystals[2].transform.localScale = new Vector3(tymeCrystals[2].transform.localScale.x, (tymeCharge - portalCloseCost*2)/portalCloseCost, tymeCrystals[2].transform.localScale.z);
         }
     }
 }
