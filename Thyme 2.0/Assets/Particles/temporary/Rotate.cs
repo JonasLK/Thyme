f29406beae6b9c31c1f3c4348d5a1b0d29773a80
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-02-28 11:54:40 +0100

    sword trail particle

---
+++ Thyme 2.0/Assets/Particles/temporary/Rotate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public bool b;
+    public Vector3 v;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(b == false)
+        {
+            transform.Rotate(v * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(-v * Time.deltaTime);
+        }
+
+        if(transform.rotation.x > 90)
+        {
+            b = true;
+        }
+        else if(transform.rotation.x < -90)
+        {
+            b = false;
+        }
+    }
+}
