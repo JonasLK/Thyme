af075d6cc7085552ba892c6e06bdb1cb24a8441b
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-03-02 16:03:07 +0100

    getting hit effect and prefab

---
+++ Thyme 2.0/Assets/Scripts/UI/Hit_Effect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hit_Effect : MonoBehaviour
+{
+    private GameObject canvas;
+    public GameObject bloodScreenImage;
+    public bool depleteAlpha;
+    private float timer;
+    public float maxTimer;
+    public float camShakeDuration, camShakeMagnitude;
+    public CamShake cameraWithCamShake;
+
+    void Start()
+    {
+        canvas = gameObject;
+        timer = maxTimer;
+    }
+
+    public void Update()
+    {
+        if (depleteAlpha == true)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if(timer < 0)
+        {
+            depleteAlpha = false;
+            timer = maxTimer;
+            bloodScreenImage.SetActive(false);
+        }
+
+        if (Input.GetKeyDown("l"))// testing purposes
+        {
+            DisplayBloodScreenImage();
+        }
+    }
+
+    public void DisplayBloodScreenImage()
+    {
+        bloodScreenImage.SetActive(true);
+        StartCoroutine(cameraWithCamShake.CustomScreenShake(camShakeDuration, camShakeMagnitude));
+        depleteAlpha = true;
+    }
+}
+/*active zetten
+ * alpha geleidelijk naar beneden
+ * dan uit
+ */
