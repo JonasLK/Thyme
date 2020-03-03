5981a4bb2cac03838e0a114051bc1cb1989c3444
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-03-03 17:32:53 +0100

    hp text

--- Thyme 2.0/Assets/Scripts/UI/PlayerHealth.cs
+++ Thyme 2.0/Assets/Scripts/UI/PlayerHealth.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     public GameObject emptyToScale;
+    public GameObject emptyToScaleMaxHp;
     public Hit_Effect hitEffect;
+    public Text healthText;
+    // player health script here
 
     //for testing
     public float maxHealth;
     public float currentHealth;
-    // player health script here
 
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateHealthText();
     }
 
     public void Update()
@@ -31,12 +34,19 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= damageTaken;
         emptyToScale.transform.localScale = new Vector3(currentHealth / maxHealth, emptyToScale.transform.localScale.y, emptyToScale.transform.localScale.z);
         hitEffect.DisplayBloodScreenImage();
-         
+        UpdateHealthText();
 
-        if(emptyToScale.transform.localScale.x < 0)
+        if(currentHealth < 0)
         {
+            currentHealth = 0;
             emptyToScale.transform.localScale = new Vector3(0, emptyToScale.transform.localScale.y, emptyToScale.transform.localScale.z);
+            UpdateHealthText();
             //death code
         }
+    }
+
+    public void UpdateHealthText()
+    {
+        healthText.text = currentHealth.ToString() + "/" + maxHealth.ToString();
     }
 }
