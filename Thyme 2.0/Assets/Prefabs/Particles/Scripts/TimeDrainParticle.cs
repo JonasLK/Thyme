23d5f8e899109a907bae3b73c49897cf8af6b07f
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-03-06 19:17:47 +0100

    lvl exp bloodScreen smokeExplosion particles

---
+++ Thyme 2.0/Assets/Prefabs/Particles/Scripts/TimeDrainParticle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDrainParticle : MonoBehaviour
+{
+    public GameObject player;
+    public bool timeDrainActive;
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+    
+    void Update()
+    {
+        if (timeDrainActive)
+        {
+            transform.LookAt(player.transform);
+            var main = ps.main;
+            main.startLifetime = Vector3.Distance(transform.position, player.transform.position) / ps.main.startSpeed.constant;
+        }
+
+        if (Input.GetKeyDown("i"))
+        {
+            timeDrainActive = true;
+        }
+    }
+}
