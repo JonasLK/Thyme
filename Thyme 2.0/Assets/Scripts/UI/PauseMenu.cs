cd3ead177e898458e728ab2b5b27befbefaee653
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-03-13 18:09:11 +0100

    function menu and corresponding prefabs

---
+++ Thyme 2.0/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool pauseMenuActive;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (pauseMenuActive == false)
+            {
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0;
+                pauseMenuActive = true;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1;
+                pauseMenuActive = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+}
