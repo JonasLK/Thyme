e1e95a75356662204471c2760ff100d6020ffeed
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-03-13 11:42:52 +0100

    SoundSettings

---
+++ Thyme 2.0/Assets/Scripts/UI/ControlVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ControlVolume : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+
+    public void SetMasterVolume(float volume)
+    {
+        audiomixer.SetFloat("MasterVolume", volume);
+    }
+    
+    public void SetMusicVolume(float volume)
+    {
+        audiomixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        audiomixer.SetFloat("SFXVolume", volume);
+    }
+}
