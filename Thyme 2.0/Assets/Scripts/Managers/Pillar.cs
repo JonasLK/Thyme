0afd077e755e8d57346b0e010776c33c8b233676
Ryusake Weiss <ryuweiss@gmail.com>
2020-03-15 16:59:24 +0100

    Done

---
+++ Thyme 2.0/Assets/Scripts/Managers/Pillar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillar : MonoBehaviour
+{
+    public float chargeTimer, chargeTimerMax;
+    public bool enabledPillar, donePillar;
+
+    public void Update()
+    {
+        if(enabledPillar && !donePillar)
+        {
+            ChargingPillar();
+        }
+    }
+
+    public void ChargingPillar()
+    {
+        if(chargeTimer < chargeTimerMax)
+        {
+            chargeTimer += Time.deltaTime;
+        }
+        else
+        {
+            donePillar = true;
+        }
+    }
+}
